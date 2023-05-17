9d61a59c15dcbf66968d7043289d490d5ed5e530
Purple-Code-sh <purple.code.sh@gmail.com>
2023-05-16 19:10:26 -0600

    function values modified after first test (all new sensors jsumo)

--- Vampiro.ino
+++ Vampiro.ino
@@ -1,37 +1,36 @@
 #include <Servo.h>
 #define Linea_Iz A0
 #define Linea_Dr A1
-#define Sen_Dr 2
-#define Sen_Efr1 4
-#define Sen_Efr2 7 
-#define Sen_Iz 8
-#define Band 10
-int Motor1_1 = 3;
-int Motor1_2 = 6;
-int Motor2_1 = 9;
-int Motor2_2 = 5;
-#define led 0
+#define Sen_Der 7
+#define Sen_Enfr_Der 10
+#define Sen_Enfr_Izq 4
+#define Sen_Izq 8
+#define Band 2
+int MotorIzq_1 = 5;
+int MotorIzq_2 = 9;
+int MotorDer_1 = 3;
+int MotorDer_2 = 6;
 #define Control A7
 Servo Bandera; 
 
 int sensorValue;
 int lineValue;
 int justOneTime;
+int ledi;
 
 void setup() {
   Serial.begin(9600);
   Bandera.attach(Band);
   pinMode(Linea_Iz, INPUT);
   pinMode(Linea_Dr, INPUT);
-  pinMode(Sen_Dr, INPUT);
-  pinMode(Sen_Efr1, INPUT);
-  pinMode(Sen_Efr2, INPUT);
-  pinMode(Sen_Iz, INPUT);
-  pinMode(Motor1_1, OUTPUT);
-  pinMode(Motor1_2, OUTPUT);
-  pinMode(Motor2_1, OUTPUT);
-  pinMode(Motor2_2, OUTPUT);
-  pinMode(led, OUTPUT);
+  pinMode(Sen_Der, INPUT);
+  pinMode(Sen_Enfr_Der, INPUT);
+  pinMode(Sen_Enfr_Izq, INPUT);
+  pinMode(Sen_Izq, INPUT);
+  pinMode(MotorIzq_1, OUTPUT);
+  pinMode(MotorIzq_2, OUTPUT);
+  pinMode(MotorDer_1, OUTPUT);
+  pinMode(MotorDer_2, OUTPUT);
 
   justOneTime = 1;
   
@@ -40,155 +39,194 @@ void setup() {
 void loop() {
   while(1){
 
-    if(justOneTime == 1){
+    /*if(justOneTime == 1){
       delay(5000);
-      justOneTime == 0;
-    }
+      justOneTime = 0;
+    }*/
     
-    Bandera.write(170);
+    //Bandera.write(25);
     sensorValue = sensorState();
-    lineValue = lineState();
+    lineValue = 0;
     
     switch(lineValue){
       case 0: //PELEA
+        //Serial.println("pelea");
         switch(sensorValue){
           case 0:
             //ninguno detecta
-            adelante(75,75);
-            ledFunc();
-            adelante(150,150);
-            ledFunc();
-            adelante(210,210);
-            ledFunc();
+            Serial.println("ninguno");
+            adelante(26,255);
+            delay(20);
+            paro();
+            delay(130);
             break;
           case 1:
             //enfrente-der
-            derecha(160, 160);
+            Serial.println("enfrente-der");
+            derecha(10, 10);
+            //paro();
             break;
           case 2:
             //enfrente-izq
-            izquierda(110, 110);
+            Serial.println("enfrente-izq");
+            izquierda(20, 20);
+            //paro();
             break;
           case 3:
             //ambos enfrente
-            adelante(75,75);
-            delay(30);
+            Serial.println("ambos enfrente");
+            adelante(1,255);
+            delay(5);
             paro();
-            delay(30);
+            delay(250);
             break;
           case 4:
             //Derecha
-            derecha(220, 220);
+            Serial.println("solo derecha");
+            derecha(18, 18);
+            //paro();
             break;
           case 5:
             //Derecha y enfrente-der
-            derecha(180, 180);
+            Serial.println("derecha y enf-der");
+            derecha(13, 13);
+            //paro();
+            break;
+          case 7:
+            // 3 a la derecha
+            derecha(8,8);
             break;
           case 8:
             //Izquierda
-            izquierda(220, 220);
+            Serial.println("solo izquierda");
+            //paro();
+            izquierda(30,30);
             break;
           case 10:
             //Izquierda y enfrente-izq
-            izquierda(130, 130);
+            Serial.println("izquierda y enf-izq");
+            //paro();
+            izquierda(16, 16);
+            break;
+          case 11:
+            // 3 a la izquierda
+            izquierda(12,12);
+            break;
+           case 12:
+            // Izquierda y derecha
+            izquierda(12,12);
+            break;
+           case 15:
+            // Todos
+            adelante(26,255);
+            delay(20);
+            paro();
+            delay(130);
             break;
         }
         break;
 
       case 1:
         //DERECHA
-        atras(200, 200);
+        Serial.println("linea der");
+        /*atras(120, 120);
         delay(400);
-        izquierda(200,200);
+        izquierda(120,120);
         delay(400);
-        adelante(100,100);
+        adelante(80,80);*/
        break;
 
       case 2:
         //IZQUIERDA
-        atras(200, 200);
-        delay(400);
-        derecha(200,200);
-        delay(400);
-        adelante(100,100);
+        Serial.println("linea izq");
+        /*atras(120, 120);
+        delay(300);
+        derecha(120,120);
+        delay(300);
+        adelante(80,80);*/
         break;
 
       case 3:
-        atras(200, 200);
-        delay(400);
-        izquierda(200,200);
-        delay(400);
-        adelante(100,100);
+        Serial.println("linea ambos");
+        /*atras(120, 120);
+        delay(300);
+        izquierda(130,130);
+        delay(300);
+        adelante(80,80);*/
         break;
     }
   }
+  
+  //paro();
+  //Bandera.write(0);
+  
 }
 
 // ---------------- Sensores ----------------
 
 int sensorState(){
-  int valor_1 = digitalRead(Sen_Efr1); 
-  int valor_2 = digitalRead(Sen_Efr2)*2;
-  int valor_Der = digitalRead(Sen_Dr)*4;
-  int valor_Iz = digitalRead(Sen_Iz)*8;
+  int valor_1 = digitalRead(Sen_Enfr_Der); 
+  int valor_2 = digitalRead(Sen_Enfr_Izq)*2;
+  int valor_Der = digitalRead(Sen_Der)*4;
+  int valor_Iz = digitalRead(Sen_Izq)*8;
 
   int suma = valor_1 + valor_2 + valor_Der + valor_Iz;
-  Serial.print("S :  ");
-  Serial.println(suma);
+  /*Serial.print("S :  ");
+  Serial.println(suma);*/
   return suma;
 }
 
 int lineState(){
-  int valine_Iz = digitalRead(Linea_Iz);
-  int valine_Dr = digitalRead(Linea_Dr)*2;
+  int valine_Iz =! digitalRead(Linea_Iz);
+  int valine_Dr =! digitalRead(Linea_Dr)*2;
 
   int suma = valine_Iz + valine_Dr;
-  Serial.print("L :  ");
-  Serial.println(suma);
+  /*Serial.print("L :  ");
+  Serial.println(suma);*/
   return suma;
 }
 
 // ---------------- Motores ----------------
 
 void adelante(int a, int b){
-  digitalWrite(Motor1_1, a);
-  digitalWrite(Motor1_2, LOW);
-  digitalWrite(Motor2_1, b);
-  digitalWrite(Motor2_2, LOW);
+  digitalWrite(MotorIzq_1, a);
+  digitalWrite(MotorIzq_2, LOW);
+  digitalWrite(MotorDer_1, b);
+  digitalWrite(MotorDer_2, LOW);
 }
 
 void derecha(int a, int b){
-  digitalWrite(Motor1_1, a);
-  digitalWrite(Motor1_2, LOW);
-  digitalWrite(Motor2_1, LOW);
-  digitalWrite(Motor2_2, b);
+  digitalWrite(MotorIzq_1, a);
+  digitalWrite(MotorIzq_2, LOW);
+  digitalWrite(MotorDer_1, LOW);
+  digitalWrite(MotorDer_2, b);
 }
 
 void izquierda(int a, int b){
-  digitalWrite(Motor1_1, LOW);
-  digitalWrite(Motor1_2, a);
-  digitalWrite(Motor2_1, b);
-  digitalWrite(Motor2_2, LOW);
+  digitalWrite(MotorIzq_1, LOW);
+  digitalWrite(MotorIzq_2, a);
+  digitalWrite(MotorDer_1, b);
+  digitalWrite(MotorDer_2, LOW);
 }
 
 void atras(int a, int b){
-  digitalWrite(Motor1_1, LOW);
-  digitalWrite(Motor1_2, a);
-  digitalWrite(Motor2_1, LOW);
-  digitalWrite(Motor2_2, b);
+  digitalWrite(MotorIzq_1, LOW);
+  digitalWrite(MotorIzq_2, a);
+  digitalWrite(MotorDer_1, LOW);
+  digitalWrite(MotorDer_2, b);
 }
 
 void paro(){
-  digitalWrite(Motor1_1, LOW);
-  digitalWrite(Motor1_2, LOW);
-  digitalWrite(Motor2_1, LOW);
-  digitalWrite(Motor2_2, LOW);
+  digitalWrite(MotorIzq_1, LOW);
+  digitalWrite(MotorIzq_2, LOW);
+  digitalWrite(MotorDer_1, LOW);
+  digitalWrite(MotorDer_2, LOW);
 }
 
 void ledFunc(){
-  if(digitalRead(led)==1){
-    digitalWrite(led, LOW);
+  if(ledi==1){
+    ledi=0;
   }else{
-    digitalWrite(led, HIGH);
+    ledi=1;
   }
 }
